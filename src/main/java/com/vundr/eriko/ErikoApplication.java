099008715d70956a919a336b884931fd6461f850
Andrey Vinel <vinel.work03@gmail.com>
2022-05-12 23:08:26 +0400

    Initial commit

---
+++ src/main/java/com/vundr/eriko/ErikoApplication.java
@@ -0,0 +1,13 @@
+package com.vundr.eriko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErikoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErikoApplication.class, args);
+    }
+
+}
