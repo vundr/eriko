d4a3b25f47d7d7922c799f0a0ac4ec88de48b4cc
Andrey Vinel <vinel.work03@gmail.com>
2022-05-15 19:52:16 +0400

    Create web application starter

---
+++ api/eriko-web/src/main/java/com/vundr/eriko/ErikoApplicationStarter.java
@@ -0,0 +1,12 @@
+package com.vundr.eriko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErikoApplicationStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErikoApplicationStarter.class);
+    }
+}
